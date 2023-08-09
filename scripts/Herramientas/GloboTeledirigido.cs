5722f3a67495b9c2a588714b1adcebb3e5519902
Daniel Jimenez <danielonoide24@gmail.com>
2023-08-08 21:21:17 -0600

    Mejorado el evitamiento del acercamiento de un globo teledirigido a un jugador

--- scripts/Herramientas/GloboTeledirigido.cs
+++ scripts/Herramientas/GloboTeledirigido.cs
@@ -4,16 +4,8 @@ using System.Collections.Generic;
 
 public class GloboTeledirigido : GloboConAgua
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-
-    // Called when the node enters the scene tree for the first time.
     readonly float speed=400;
     bool exploded=false;
-
-    float angle=0;
-    List<Jugador> playersInRange = new();
 
     AudioStreamPlayer2D startingSound;
 
@@ -35,70 +27,25 @@ public class GloboTeledirigido : GloboConAgua
         velocity.x=Convert.ToInt32(Input.IsActionPressed("Right")) - Convert.ToInt32(Input.IsActionPressed("Left"));
 		velocity.y=Convert.ToInt32(Input.IsActionPressed("Down")) - Convert.ToInt32(Input.IsActionPressed("Up"));
         
-/*         float movementAngle=Mathf.Rad2Deg(velocity.Angle());
-        var player=GetClosestPlayer();
-        angle=player is null ? 0 : Mathf.Rad2Deg((player.GlobalPosition-GlobalPosition).Angle());
-
-        if(angle!=0 && movementAngle>=angle-50 && movementAngle<=angle+50)
-        {
-            Vector2 drawBack=(player.GlobalPosition.DirectionTo(GlobalPosition));
-            drawBack*=20;
-            velocity=drawBack;
-            MoveAndSlide(velocity);
-            return;
-        } */
 
         if(Input.IsActionPressed("LeftClick"))
         {
             velocity=Position.DirectionTo(GetGlobalMousePosition()).Normalized();
         }
 
-        float movementAngle=Mathf.Rad2Deg(velocity.Angle());
-        CalculateAverageAngle();
-
-        if(angle!=0 && movementAngle>=angle-50 && movementAngle<=angle+50)
-        {
-            return;
-        }
 
 		velocity.x*=speed;
 		velocity.y*=speed;
 
 
-        velocity=MoveAndSlide(velocity, Vector2.Up);
-    }
-    private void CalculateAverageAngle()
-    {
-        if (playersInRange.Count == 0)
+        //velocity=MoveAndSlide(velocity, Vector2.Up);
+
+        var collisionInfo= MoveAndCollide(velocity*delta, testOnly:true);
+
+        if(collisionInfo==null)
         {
-            angle = 0;
-            return;
+            MoveAndSlide(velocity);
         }
-
-        float totalAngle = 0;
-        foreach (Jugador player in playersInRange)
-        {
-            totalAngle += player.GlobalPosition.AngleToPoint(GlobalPosition);
-        }
-
-        angle = Mathf.Rad2Deg(totalAngle / playersInRange.Count);
-    }
-    private Jugador GetClosestPlayer()
-    {
-        float closestDistance = float.MaxValue;
-        Jugador closestPlayer = null;
-
-        foreach (Jugador player in playersInRange)
-        {
-            float distance = GlobalPosition.DistanceSquaredTo(player.GlobalPosition);
-            if (distance < closestDistance)
-            {
-                closestDistance = distance;
-                closestPlayer = player;
-            }
-        }
-
-        return closestPlayer;
     }
 
     protected new void _on_Collision_body_entered(Node body)
@@ -121,23 +68,6 @@ public class GloboTeledirigido : GloboConAgua
             jugador.AddHumidity(GetHumidityPoints(distance));
             Push(jugador, distance);
             Escenario.AddStar(jugador.IsMartian, true);
-        }
-    }
-
-
-    private void _on_Detector_body_entered(Node body)
-    {
-        if(body is Jugador player)
-        {
-            playersInRange.Add(player);
-        }
-    }
-
-    private void _on_Detector_body_exited(Node body)
-    {
-        if(body is Jugador jugador)
-        {
-            playersInRange.Remove(jugador);
         }
     }
 
