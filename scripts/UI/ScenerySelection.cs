e7777729b4cdc92d49d18411bee999b7f0c6beef
Daniel Jimenez <danielonoide24@gmail.com>
2023-09-15 10:23:35 -0600

    ahora se desactiva el boton de continuar justo despues de empezar nueva partida

--- scripts/UI/ScenerySelection.cs
+++ scripts/UI/ScenerySelection.cs
@@ -129,6 +129,8 @@ public class ScenerySelection : CanvasLayer
 
         InventorySelection inventorySelection=InventorySelection.GetInventorySelection(scenery);
         AddChild(inventorySelection);   
+        DisableContinueButtons();
+
     }
 
 
