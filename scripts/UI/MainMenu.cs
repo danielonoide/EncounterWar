0528b7a5f5649ddc47e44ac2eb6dfb90a299b47d
Daniel Jimenez <danielonoide24@gmail.com>
2023-09-17 19:43:12 -0600

    limpieza código main menu

--- scripts/UI/MainMenu.cs
+++ scripts/UI/MainMenu.cs
@@ -4,76 +4,53 @@ using System;
 
 public class MainMenu : CanvasLayer
 {
-	Vector2 Movimiento=new Vector2(1,0);
-	
-	Vector2 OpEscalaSec=new Vector2((float)0.5,(float)0.5);
-	Vector2 OpEscalaIni=new Vector2((float)0.3,(float)0.3);	
-	Vector2 OpPosicionSec=new Vector2(2340,1132);
-	Vector2 OpPosicionIni=new Vector2(1222,609);
-	
-	Vector2 StEscalaSec=new Vector2((float)1.2,(float)1.2);
-	Vector2 StEscalaIni=new Vector2(1,1);	
-	Vector2 StPosicionSec=new Vector2(443,370);
-	Vector2 StPosicionIni=new Vector2(480,376);
-	
-	Vector2 ExEscalaIni=new Vector2((float)0.8,(float)0.8);	
-	Vector2 ExPosicionSec=new Vector2(480,489);
-	Vector2 ExPosicionIni=new Vector2(516,500);
-	
-	
-	//public ParallaxLayer Bg;
-	Camera2D Camara;
-	TextureButton Start, Opciones, Exit, tutorialsButton;
+	readonly Vector2 movimiento=new(1,0);
+
+	Camera2D camara;
+	TextureButton start, opciones, exit, tutorialsButton;
+
+    readonly Vector2 scaling = new(0.2f, 0.2f);
 	
 	
 	public override void _Ready()
 	{
-		//Bg=GetNode("ParallaxBackground").GetNode<ParallaxLayer>("Bg");
-		Camara=GetNode<Camera2D>("Camera2D");
-		Start=GetNode<TextureButton>("Start");
-		Exit=GetNode<TextureButton>("Exit");		
-		Opciones=GetNode<TextureButton>("Opciones");
+		camara=GetNode<Camera2D>("Camera2D");
+		start=GetNode<TextureButton>("Start");
+		exit=GetNode<TextureButton>("Exit");		
+		opciones=GetNode<TextureButton>("Opciones");
 		tutorialsButton=GetNode<TextureButton>("TutorialsBTN");
 
+		var buttons = GetTree().GetNodesInGroup("Buttons");
+
+		foreach(TextureButton button in buttons)
+		{
+			button.Connect("mouse_entered", this, nameof(OnButtonMouseEntered), new Godot.Collections.Array{button});
+			button.Connect("mouse_exited", this, nameof(OnButtonMouseExited), new Godot.Collections.Array{button});			
+		}
 
 		Input.SetCustomMouseCursor(null);
 	}
 
-
-  public override void _PhysicsProcess(float delta)
-  {
-	/*if(GetNode<Sprite>("Bg").Position.y==514) 
+	public override void _PhysicsProcess(float delta)
 	{
-		Movimiento.x=-1;
-		Movimiento.y=-1;
-	}
-	if(GetNode<Sprite>("Bg").Position.y==222) 
-	{
-		Movimiento.x=1;
-		Movimiento.y=1;
-	}
-	*/
-	
-	//Bg.Position+=Movimiento;
-	Camara.Position+=Movimiento;
-  }
-
-	private void _on_Opciones_mouse_entered()
-	{
-		Modify.ChangeScale(Opciones, OpEscalaSec);
-		//Modify.ChangePosition(Opciones, OpPosicionSec);
+		camara.Position+=movimiento;
 	}
 
-	private void _on_Opciones_mouse_exited()
+	private void OnButtonMouseEntered(TextureButton textureButton)
 	{
-		Modify.ChangeScale(Opciones, OpEscalaIni);
-		//Modify.ChangePosition(Opciones, OpPosicionIni);
+		textureButton.RectScale+=scaling;
 	}
+
+	private void OnButtonMouseExited(TextureButton textureButton)
+	{
+		textureButton.RectScale-=scaling;
+	}
+
 	
 	private void _on_Opciones_pressed()
 	{
 		AddChild(Settings.GetSettings());
-		Opciones.Hide();
+		opciones.Hide();
 	}
 
 	private void _on_Tutorials_pressed()
@@ -81,46 +58,12 @@ public class MainMenu : CanvasLayer
 		AddChild(Tutorials.GetTutorials());
 	}
 
-	private void _on_Tutorials_mouse_entered()
-	{
-		Modify.ChangeScale(tutorialsButton, OpEscalaSec);
-	}
-
-	private void _on_Tutorials_mouse_exited()
-	{
-		Modify.ChangeScale(tutorialsButton, OpEscalaIni);
-	}
-	
 	private void CloseSettings()
 	{
-		Modify.ChangeScale(Opciones, OpEscalaIni);
-		Opciones.Show();
+		opciones.RectScale-=scaling;
+		opciones.Show();
 	}
-	
-	private void _on_Start_mouse_entered()
-	{
-		Modify.ChangeScale(Start, StEscalaSec);
-		Modify.ChangePosition(Start, StPosicionSec);
-	}
-	
-	private void _on_Start_mouse_exited()
-	{
-		Modify.ChangeScale(Start, StEscalaIni);
-		Modify.ChangePosition(Start, StPosicionIni);
-	}
-	
-	private void _on_Exit_mouse_entered()
-	{
-		Modify.ChangeScale(Exit, StEscalaIni);
-		Modify.ChangePosition(Exit, ExPosicionSec);
-	}
-	
-	private void _on_Exit_mouse_exited()
-	{
-		Modify.ChangeScale(Exit, ExEscalaIni);
-		Modify.ChangePosition(Exit, ExPosicionIni);
-	}
-	
+
 	private void _on_Start_pressed()
 	{
 		AddChild(ScenerySelection.GetScenerySelection());
